d7d75c49f9b7b5a6371351628d6a82f74d677984
Masmoudi1 <masmoudiousema9@gmail.com>
2021-12-03 01:45:23 +0100

    first commit

---
+++ src/main/java/com/example/nuitdinfo/NuitDinfoApplication.java
@@ -0,0 +1,13 @@
+package com.example.nuitdinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NuitDinfoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NuitDinfoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/nuitdinfo/NuitDinfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nuitdinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NuitDinfoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
